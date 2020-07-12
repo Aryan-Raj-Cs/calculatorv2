ea50c516da8a10cce2e9df297d90afbaab0ca423
Aryan-Raj-Cs <aryanrajresonance@gmail.com>
2020-07-12 11:10:15 +0530

    Updates

---
+++ precache-manifest.fcb4df948a54cae62b4bed4a85874e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35603d5a49ef7f80b3e511567f2d1343",
+    "url": "/calculatorv2/index.html"
+  },
+  {
+    "revision": "e9bf565405725b008fb4",
+    "url": "/calculatorv2/static/css/main.bcd70ac3.chunk.css"
+  },
+  {
+    "revision": "8a1ac6de1de9acf59937",
+    "url": "/calculatorv2/static/js/2.bb6b61de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculatorv2/static/js/2.bb6b61de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9bf565405725b008fb4",
+    "url": "/calculatorv2/static/js/main.dc90b6bc.chunk.js"
+  },
+  {
+    "revision": "79612e9dabbb3f9f6020",
+    "url": "/calculatorv2/static/js/runtime-main.b5588d79.js"
+  }
+]);
